98101242862356e723611046a0649f6c939cf038
Eero af Heurlin <eero.afheurlin@iki.fi>
2019-06-30 03:10:36 +0300

    new shell script to start the two separate processes

---
+++ reactorconsole/reactorconsole.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pkill -f -INT backendcoms.py
+pkill -f -INT backendcoms.py
+pkill -f -INT reactorlocal.py
+pkill -f -INT reactorlocal.py
+
+source /home/hacklab/.virtualenvs/reactorconsole/bin/activate
+cd ~/devel/odysseus-python/reactorconsole
+./backendcoms.py --id jump_reactor --url http://192.168.1.2 &
+./reactorlocal.py
